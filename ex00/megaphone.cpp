9c6e90ff7ea38db65b53b6800b8caaa538ec4b19
Maksim Volkmann <mvolkman@2-H-9.42heilbronn.de>
2024-08-16 16:52:25 +0200

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mvolkman <mvolkman@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 14:49:12 by mvolkman          #+#    #+#             */
-/*   Updated: 2024/08/15 18:38:57 by mvolkman         ###   ########.fr       */
+/*   Updated: 2024/08/16 15:59:59 by mvolkman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,21 @@
 int	main(int ac, char **av)
 {
 	if(ac == 1)
+	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
 	else
 	{
-		std::cout << "Just testing +1 arguments" << std::endl;
+		std::string output;
+
+		for(int i = 1; i < ac; i++)
+			output += av[i];
+
+		for(int i = 0; i < output.length(); i++)
+			output[i] = std::toupper(output[i]);
+
+		std::cout << output << std::endl;
 	}
-	
+
 	return (0);
-}
-
-
-#include <iostream>  // This header file allows us to use std::cout and other input/output functionality
-#include <cctype>    // This header file provides functions for character handling, such as std::toupper
-
-int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    } else {
-        for (int i = 1; i < argc; i++) {
-            std::string str = argv[i];  // Convert C-style string to C++ string for easier manipulation
-            for (size_t j = 0; j < str.size(); j++) {
-                std::cout << static_cast<char>(std::toupper(str[j]));  // Convert each character to uppercase
-            }
-        }
-        std::cout << std::endl;
-    }
-    return 0;  // Return 0 to indicate successful execution
 }
