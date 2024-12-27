162c9f33cbc0a7a2d77891113bfe86ae0c101272
Maksim Volkmann <mvolkman@2-H-9.42heilbronn.de>
2024-12-27 20:52:37 +0100

    subject files update

--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -11,16 +11,30 @@ private:
 	int grade;
 
 public:
-	Bureaucrat(); // Default constructor
+	Bureaucrat(); // default constructor
 	Bureaucrat(const std::string& name, int grade); // Parameterized constructor
 	Bureaucrat(const Bureaucrat& other); // Copy constructor
 	Bureaucrat& operator=(const Bureaucrat &other); // Copy assignment operator
 	~Bureaucrat(); // Destructor
 
-	// Getters
+	//getters
 	const std::string& getName() const;
 	int getGrade() const;
+
+	void incrementGrade();
+	void decrementGrade();
+
+	class GradeTooHighException : public std::exception {
+		const char* what() const noexcept override;
+	};
+
+	class GradeTooLowException : public std::exception {
+		const char* what() const noexcept override;
+	};
+
+
 };
 
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
 
 #endif
